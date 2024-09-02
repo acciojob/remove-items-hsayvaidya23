80c59e4ef2d58ede155877c4e9629a9c88bdf505
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-02 18:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const color = document.getElementById('colorSelect')
+const removeCol = document.querySelector('input[type="button"]')
+
+removeCol.addEventListener('click', () => {
+	const select = color.select;
+
+	if(select !== -1) {
+		color.remove(select);
+	}
+})
